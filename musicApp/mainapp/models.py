127529ae78ec80b86cf317fd2adf83b648782d2d
Алимхан Бакдаулет <a.bakdaulet17kz@gmail.com>
2021-04-27 16:19:41 +0600

    Resolving conflicts

--- musicApp/mainapp/models.py
+++ musicApp/mainapp/models.py
@@ -7,3 +7,23 @@ class UserManage(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Album(models.Model):
+    artist = models.CharField(max_length=100)
+    album_title = models.CharField(max_length=50)
+    genre = models.CharField(max_length=50)
+    released_year = models.IntegerField()
+
+    def __str__(self):
+        return self.album_title + '-' + self.artist
+
+
+class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    song_title = models.CharField(max_length=100, default='')
+    genre = models.CharField(max_length=50)
+    is_favorite = models.BooleanField(default=False)
+
+    def Song(self):
+        return self.song_title
