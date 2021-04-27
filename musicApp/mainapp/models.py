0873a008816ea6894002fb679f98967b9fefc761
muratbekauyez <55730136+muratbekauyez@users.noreply.github.com>
2021-04-27 11:08:13 +0600

    Update models.py

--- musicApp/mainapp/models.py
+++ musicApp/mainapp/models.py
@@ -5,7 +5,7 @@ class Album(models.Model):
     artist = models.CharField(max_length=100)
     album_title = models.CharField(max_length=50)
     genre = models.CharField(max_length=50)
-    album_logo = models.CharField(max_length=900)
+    released_year = models.IntegerField()
 
     def __str__(self):
         return self.album_title + '-' + self.artist
@@ -13,8 +13,8 @@ class Album(models.Model):
 
 class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
-    file_type = models.CharField(max_length=10)
     song_title = models.CharField(max_length=100, default='')
+    genre = models.CharField(max_length=50)
     is_favorite = models.BooleanField(default=False)
 
     def Song(self):
