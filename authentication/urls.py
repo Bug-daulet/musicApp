6e0d47dc6f039a6209a7466ba3c632814974eaf1
babyl01 <baurzhan634@gmail.com>
2021-06-16 10:11:30 +0600

    rebase

--- authentication/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-# Add URLConf
-urlpatterns = [
-    path('login/', views.login_request, name='login'),
-    path('signup/', views.signup_request, name='signup'),
-    path('logout/', views.logout_request, name='logout'),
-]
